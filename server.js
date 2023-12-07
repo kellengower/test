8514e622cb4da609be364857843d60d40b841541
kellengower <kellen.gower@edu.sait.ca>
2023-12-07 12:21:26 -0700

    first commit

--- server.js
+++ server.js
@@ -9,10 +9,11 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('CPSY 350 Project: Github Actions CI. SAIT ID:000123456');
+  res.send('CPSY 350 Project: Github Actions CI. SAIT ID:000737532');
 });
 
 const server = app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
 
 module.exports = server;
+tuna fish
